09882a83b7882812f9cad157c4d9ebdd30ee76bf
kasparreinert <146737942+kasparreinert@users.noreply.github.com>
2023-11-04 08:53:02 +0800

    add resource
    add client/resource

---
+++ - Move Compiles.bat
@@ -0,0 +1,9 @@
+@echo off
+COPY "..\source\Source\all\Debug\AccountServer.exe" "Program\1. Account.exe"
+COPY "..\source\Source\all\Debug\CacheServer.exe" "Program\6. Cache.exe"
+COPY "..\source\Source\all\Debug\Certifier.exe" "Program\2. Certifier.exe"
+COPY "..\source\Source\all\Debug\CoreServer.exe" "Program\4. Core.exe"
+COPY "..\source\Source\all\Debug\DatabaseServer.exe" "Program\3. Database.exe"
+COPY "..\source\Source\all\Debug\LoginServer.exe" "Program\5. Login.exe"
+COPY "..\source\Source\all\Debug\WorldServer.exe" "Program\7. World.exe"
+COPY "..\source\Source\all\Debug\Neuz.exe" "Resource\Neuz.exe"
